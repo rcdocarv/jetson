4358f3a560b3bc5fb4529f08d93486395d23a539
rcdocarv <rcdocarv@gmail.com>
2025-03-14 09:56:50 +0000

    Update jetson_config.sh

--- jetson_config.sh
+++ jetson_config.sh
@@ -92,6 +92,24 @@ git checkout 4.5.5
 cd ~/opencv
 mkdir build
 cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+      -D CMAKE_INSTALL_PREFIX=/usr/local \
+      -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+      -D WITH_CUDA=ON \
+      -D CUDA_ARCH_BIN="5.3" \
+      -D WITH_CUDNN=ON \
+      -D OPENCV_DNN_CUDA=ON \
+      -D ENABLE_FAST_MATH=1 \
+      -D CUDA_FAST_MATH=1 \
+      -D WITH_CUBLAS=1 \
+      -D OPENCV_GENERATE_PKGCONFIG=ON \
+      -D BUILD_EXAMPLES=OFF ..
+
+make -j4
+sudo make install
+sudo ldconfig
+import cv2
+print(cv2.getBuildInformation())
 
 # Instalar Opencv setuptools whell 
 python -m pip install --upgrade pip setuptools wheel
