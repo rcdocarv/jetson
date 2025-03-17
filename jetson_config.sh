f13a72301a00f40ad6c17a6f51c359733841302c
rcdocarv <rcdocarv@gmail.com>
2025-03-17 12:26:44 +0000

    Update jetson_config.sh

--- jetson_config.sh
+++ jetson_config.sh
@@ -57,15 +57,17 @@ nvcc --version
 
 # instalar SDK setereolabs
 sudo apt install -y build-essential python3-dev python3-pip python3-setuptools python3-wheel cython3 libatlas-base-dev libopenblas-dev
+pip3 install --upgrade pip
 pip3 install Cython==0.29.24
 pip3 install numpy==1.21.4
 sudo apt install zstd
 apt-cache policy nvidia-jetpack # consulta a versão do jetpack 4.6.6
 wget https://stereolabs.sfo2.cdn.digitaloceanspaces.com/zedsdk/4.2/ZED_SDK_Tegra_L4T32.7_v4.2.5.zstd.run
 chmod +x ZED_SDK_Tegra_L4T32.7_v4.2.5.zstd.run
-echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc && source ~/.bashrc
 ./ZED_SDK_Tegra_L4T32.7_v4.2.5.zstd.run
-
+echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc && source ~/.bashrc
+cd /usr/local/zed/
+pip install pyzed-4.2-cp38-cp38-linux_aarch64.whl
 
 # INstalação do TensorRT
 dpkg -l | grep nvinfer # Verificar Se o tensorRT está instalado
