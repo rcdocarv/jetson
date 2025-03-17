819ce06b5053e0461ddca877aad9ce9417f01b82
rcdocarv <rcdocarv@gmail.com>
2025-03-17 09:55:57 +0000

    Update jetson_config.sh

--- jetson_config.sh
+++ jetson_config.sh
@@ -76,9 +76,6 @@ sudo apt install python3.8 python3.8-dev python3.8-venv
 sudo apt install -y libjpeg-dev libpng-dev libtiff-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev
 sudo apt install -y libgtk-3-dev libcanberra-gtk3-module libatlas-base-dev gfortran
 sudo apt install -y python3.8-distutils python3.8-dev
-apt-get install python3.8-venv
-python -m venv jet
-source jet/bin/activate
 pip install --upgrade pip
 pip install opencv-python
 pip install opencv-contrib-python
