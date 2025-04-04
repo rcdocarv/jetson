8e5d0156dc74a624e2725494d41eca060086dd4c
rcdocarv <rcdocarv@gmail.com>
2025-04-04 17:22:19 +0100

    Update install.sh

--- powerbimongo/install.sh
+++ powerbimongo/install.sh
@@ -5,4 +5,11 @@ wget https://media.datadirect.com/download/files/evals/ODBC/PROGRESS_DATADIRECT_
 tar xvzf PROGRESS_DATADIRECT_ODBC_MONGODB_LINUX_64.tgz
 sudo apt-get install unixodbc
 sudo apt-get update && sudo apt install openjdk-17-jdk
+java -version
+sudo nano /etc/profile
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+source /etc/profile
+cd pbi
+sudo ./PROGRESS_DATADIRECT_ODBC_8.0_LINUX_64_INSTALL.bin
 ```
