66aac8578af6c40da446825a81735de98a383f4b
rcdocarv <rcdocarv@gmail.com>
2025-04-04 16:03:12 +0100

    Update install.sh

--- powerbimongo/install.sh
+++ powerbimongo/install.sh
@@ -1,4 +1,4 @@
-shell ´´´
+´´´ shell
 curl -LO https://info-mongodb-com.s3.amazonaws.com/mongodb-bi/v2/mongodb-bi-linux-x86_64-debian12-v2.14.14.tgz
 curl -LO https://info-mongodb-com.s3.amazonaws.com/mongodb-bi/v2/mongodb-bi-linux-x86_64-debian12-v2.14.14.tgz.sig
 gpg --verify mongodb-bi-linux-x86_64-debian12-v2.14.14.tgz.sig mongodb-bi-linux-x86_64-debian12-v2.14.14.tgz
