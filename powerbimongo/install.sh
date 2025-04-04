a7e483a57fab003b4d88f828aa8c12b18eaabb76
rcdocarv <rcdocarv@gmail.com>
2025-04-04 16:28:32 +0100

    Update install.sh

--- powerbimongo/install.sh
+++ powerbimongo/install.sh
@@ -1,5 +1,8 @@
 ``` shell 
 mkdir pbi
+cd pbi
 wget https://media.datadirect.com/download/files/evals/ODBC/PROGRESS_DATADIRECT_ODBC_MONGODB_LINUX_64.tgz
+tar xvzf PROGRESS_DATADIRECT_ODBC_MONGODB_LINUX_64.tgz
+sudo apt-get install unixodbc
 
 ```
